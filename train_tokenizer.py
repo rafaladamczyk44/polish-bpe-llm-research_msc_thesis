397eeadfa803f4fb4196d90e067079ed6681fb6a
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-08-01 15:08:40 +0200

    Training pipeline on pre-processed text

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,36 +1,20 @@
-from datasets import load_dataset
+import pickle
 from tokenizer_pl import BPETokenizerPL
-import random
-import re
-
 
 VOCAB_SIZE = 30000
-TOKENIZER_PATH = "tokenizers_vocab/"
+BATCH_SIZE = 5000
+SAMPLE_SIZE = 1000000
+MIN_FREQ = 2
+TOKENIZER_PATH = "training/tokenizer/vocab"
 
-# Download the dataset
-ds = load_dataset("chrisociepa/wikipedia-pl-20230401")
-
-# Filter your dataset
-training_data =  ds['train']['text']
-training_data = [text for text in training_data if len(text.strip()) > 50]
+with open('training/data/filtered_polish_wikipedia.pkl', 'rb') as f:
+    training_data = pickle.load(f)
 
 # Initialize the Polish and rain the Polish tokenizer
 tokenizer_pl = BPETokenizerPL(vocab_size=VOCAB_SIZE)
-print('Starting the training')
 
-tokenizer_pl.train(training_data, sample_size=1000000, verbose=True)
+# Start the training
+tokenizer_pl.train(training_data, sample_size=SAMPLE_SIZE, min_word_freq=MIN_FREQ, batch_size=BATCH_SIZE)
 
 # Save to disk
 tokenizer_pl.save_vocab(TOKENIZER_PATH)
-
-"""
-# Run evaluation
-tokenizer.calculate_coverage(test_corpus)
-tokenizer.calculate_fertility(test_corpus)
-test_words = ["mógłbym", "Polski", "kotami", "napisać"]
-for word in test_words:
-    tokens = tokenizer.tokenize(word)
-    print(f"{word} → {' | '.join(tokens)}")
-"""
-
-
