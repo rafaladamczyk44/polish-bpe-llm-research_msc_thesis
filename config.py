ca0905bf7b3a36d9d7a961b25afa940667a51ce1
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-08-03 14:21:14 +0200

    Adding model configurations

---
+++ config.py
@@ -0,0 +1,24 @@
+class Config25M:
+    vocab_size = 30000
+    d_model = 256
+    num_heads = 8
+    num_layers = 4
+    context_size = 512
+    d_ff = 4*d_model
+
+class Config70M:
+    vocab_size = 30000
+    d_model = 512
+    num_heads = 8
+    num_layers = 6
+    context_size = 512
+    d_ff = 4*d_model
+
+class Config85M:
+    vocab_size = 30000
+    d_model = 768
+    num_heads = 12
+    num_layers = 6
+    context_size = 512
+    d_ff = 4*d_model
+
--- train.py
+++ train.py
@@ -7,9 +7,27 @@ import matplotlib.pyplot as plt
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 
+from tokenizer_pl import BPETokenizerPL
+from language_model import LanguageModel
+from config import *
+
 run = wandb.init(
     project="master_thesis",
 )
 
 wandb_logger = WandbLogger()
 trainer = L.Trainer(max_epochs=10, logger=wandb_logger)
+
+config = Config25M
+
+tokenizer_pl = BPETokenizerPL(config.vocab_size)
+
+model = LanguageModel(
+    vocab_size=config.vocab_size,
+    d_model=config.d_model,
+    num_heads=config.num_heads,
+    num_layers=config.num_layers,
+    context_size=config.context_size,
+    d_ff=config.d_ff,
+    dropout=0.1,
+)
