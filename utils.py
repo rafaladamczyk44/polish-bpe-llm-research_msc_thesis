8b8202759abe13554599c6c2ef0d044d4fa01bc8
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-08-04 15:06:35 +0200

    Adding collate_fn for padding

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import math
+import pickle
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
 from attention import MultiHeadAttention
 
 class PositionalEncoding(nn.Module):
@@ -75,3 +77,28 @@ class TransformerBlock(nn.Module):
         x = self.layer_norm2(x)
 
         return x
+
+class WikiDataset(Dataset):
+    def __init__(self, path):
+        self.data = self._load_pkl(path=path)
+
+    def __getitem__(self, index):
+        return torch.tensor(self.data[index], dtype=torch.long)
+
+    def __len__(self):
+        return len(self.data)
+
+    def _load_pkl(self, path):
+        """Load tokenized(!) data"""
+        with open(path, 'rb') as f:
+            training_data = pickle.load(f)
+
+        return training_data
+
+
+def collate_fn(batch):
+    """
+    Custom collate function to handle variable-length sequences
+    Pads sequences to the same length within each batch
+    """
+    return pad_sequence(batch, batch_first=True, padding_value=0)
