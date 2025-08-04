f9f7c0631999114c4181563a99e9e220a5faf71b
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-08-04 18:50:57 +0200

    Updating to the correct vocab size

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+VOCAB_SIZE = 30006
+
 class Config18M:
-    vocab_size = 30000
+    vocab_size = VOCAB_SIZE
     d_model = 256
     num_heads = 8
     num_layers = 4
@@ -7,7 +9,7 @@ class Config18M:
     d_ff = 4*d_model
 
 class Config70M:
-    vocab_size = 30000
+    vocab_size = VOCAB_SIZE
     d_model = 512
     num_heads = 8
     num_layers = 6
@@ -15,7 +17,7 @@ class Config70M:
     d_ff = 4*d_model
 
 class Config85M:
-    vocab_size = 30000
+    vocab_size = VOCAB_SIZE
     d_model = 768
     num_heads = 12
     num_layers = 6
